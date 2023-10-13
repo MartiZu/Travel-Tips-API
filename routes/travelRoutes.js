ad9dd46b265d913cf6f642250d06959ca61feccb
MartiZu <martina.zurli@gmail.com>
2023-10-13 01:01:22 +0100

    added routes

---
+++ routes/travelRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import * as travelController from "../controllers/travelController.js";
+
+export const travelRoutes = express.Router();
+
+travelRoutes.get("/", travelController.getTravels);
+
+travelRoutes.get("/:id", travelController.getTravelById);
+
+travelRoutes.post("/", travelController.createTravel);
+
+travelRoutes.patch("/:id", travelController.updateTravelByID);
+
+travelRoutes.delete("/:id", travelController.deleteTravelByID);
